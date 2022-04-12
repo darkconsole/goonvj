b6cf73f2929a35e47294ad39bad9f4f9450cb5b7
darkconsole <darkcee.legit@gmail.com>
2022-04-12 00:38:12 -0500

    remove electron reloader ref

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import ElectronReloader from 'electron-reloader';
 import Electron from 'electron';
 import GoonVJ from './GoonVJ/Server/Main';
-
-ElectronReloader(module);
 
 Electron.app.on('ready', function(){
 	const server = new GoonVJ(__dirname);
